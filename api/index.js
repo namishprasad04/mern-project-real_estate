ea2a6df6843c51240ca7b7bff021942f9873bd33
Namish Prasad <namishprasad180@gmail.com>
2024-08-09 23:46:13 +0530

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -17,7 +18,9 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
