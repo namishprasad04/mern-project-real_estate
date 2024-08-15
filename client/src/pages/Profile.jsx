b827ba9bc0476a4c111310152cf824dc8c127db7
Namish Prasad <namishprasad180@gmail.com>
2024-08-15 19:06:21 +0530

    completed delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -20,7 +20,7 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState("");
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
-  const [showListings, setShowLisitngs] = useState(false);
+  const [showListings, setShowListings] = useState(false);
   const [userListings, setUserListings] = useState([]);
 
   const dispatch = useDispatch();
@@ -140,17 +140,35 @@ export default function Profile() {
 
   const handleShowListing = async () => {
     try {
-      setShowLisitngs(false);
+      setShowListings(false);
       const res = await fetch(`/api/user/listings/${currentUser._id}`);
       const data = await res.json();
       if (data.success === false) {
-        setShowLisitngs(true);
+        setShowListings(true);
         return;
       }
       setUserListings(data);
     } catch (error) {
-      setShowLisitngs(true);
+      setShowListings(true);
       console.error(error);
+    }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -242,26 +260,42 @@ export default function Profile() {
       <p className="text-red-700 mt-5">
         {showListings ? "Error showing listings" : ""}
       </p>
-      {userListings &&
-        userListings.length > 0 &&
-        userListings.map((listing) => (
-          <div key={listing._id} className="flex justify-between items-center gap-4 border rounded-lg p-3">
-            <Link to={`/listing/${listing._id}`}>
-              <img
-                src={listing.imageUrls[0]}
-                alt="lisitng cover"
-                className="h-16 w-16 object-contain"
-              />
-            </Link>
-            <Link className="flex-1 text-slate-700 font-semibold hover:underline truncate" to={`/listing/${listing._id}`}>
-              <p>{listing.name}</p>
-            </Link>
-            <div className="flex flex-col items-center">
-              <button className="text-red-700 uppercase">Delete</button>
-              <button className="text-green-700 uppercase">Edit</button>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="flex justify-between items-center gap-4 border rounded-lg p-3"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="lisitng cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="flex-1 text-slate-700 font-semibold hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+      )}
     </div>
   );
 }
