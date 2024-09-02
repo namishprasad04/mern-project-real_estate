a2dddc7bae045af38164c40fe4d0b01a395fbc42
Namish Prasad <namishprasad180@gmail.com>
2024-09-02 16:55:33 +0530

    create searchApi route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { createListing, deleteListing, getListing, updateListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getListing, getListings, updateListing } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post("/create",verifyUser,createListing);
 router.delete("/delete/:id",verifyUser,deleteListing);
 router.post("/update/:id",verifyUser,updateListing);
 router.get("/get/:id",getListing);
+router.get("/get",getListings);
+
 
 export default router;
