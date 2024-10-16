0f186a599bf02a3fb33a49f72bf037327532857e
Namish Prasad <namishprasad180@gmail.com>
2024-10-16 21:58:31 +0530

    url changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
